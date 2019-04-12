614227201b93bf3987ea6139e6fdd01dc47bce24
chigaimasu <chigaimasu@gmail.com>
2019-04-12 17:35:46 -0400

    added basic coin chest support

--- Tracker.js
+++ Tracker.js
@@ -2,12 +2,17 @@
 function GenerateChests(){
     var fieldNames = ["Surface","Gate of Guidance"];
     var lootChestCounts = [7,9];
+    var coinChestCounts =[3,2];
     for(var i=0; i<fieldNames.length; i++){
         document.getElementById("main").innerHTML += "<div id=\""+fieldNames[i]+"\">";
         document.getElementById("main").innerHTML += "<h2 class=\"fieldname\" id=\""+fieldNames[i]+"\">"+fieldNames[i]+"</h2>";
         for(var j=0; j<lootChestCounts[i]; j++){
             console.debug("looping");
             document.getElementById("main").innerHTML += "<img src=\"chest.png\" id=\""+fieldNames[i]+j+"\" class=\"unlit-chest\" />";
+        }
+        for(var j=0; j<coinChestCounts[i]; j++){
+            console.debug("looping");
+            document.getElementById("main").innerHTML += "<img src=\"coinchest.png\" id=\""+fieldNames[i]+j+"\" class=\"unlit-chest\" />";
         }
         document.getElementById("main").innerHTML += "</div>";
     }
