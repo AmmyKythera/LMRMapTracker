a1fe31b73835f862f96a10e161c564a374861db4
chigaimasu <chigaimasu@gmail.com>
2019-04-15 18:26:25 -0400

    Consolidated to single-chest countdown

--- Tracker.js
+++ Tracker.js
@@ -3,14 +3,14 @@ function generateChests(){
     
     for(var field in chestData.fields){
         var html = "";
-        html += "<div id=\""+chestData.fields[field].name+"\">";
-        html += "<h2 class=\"fieldname\" id=\""+chestData.fields[field].name+"\">"+chestData.fields[field].name+"</h2>";
+        html += "<div id=\""+field+"\">";
+        html += "<h2 class=\"fieldname\" id=\""+field+"\">"+field+"</h2>";
         
-        html += "<img src=\"chest.png\" id=\""+chestData.fields[field].name.replace(/ /g,"")+"\" class=\"lit-chest\" />";
-        html += "<div class=\"num-box\">"+(chestData.fields[field].lootChests-chestData.fields[field].currChests)+"</div>";
+        html += "<img src=\"chest.png\" id=\""+field.replace(/ /g,"")+"\" class=\"lit-chest\" />";
+        html += "<div class=\"num-box\">"+chestData.fields[field].currChests+"</div>";
         /*for(var j=0; j<chestData.fields[field].lootChests; j++){
             //Have to use a regex for replace because reasons
-            html += "<img src=\"chest.png\" id=\""+chestData.fields[field].name.replace(/ /g,"")+j+"\" class=\"unlit-chest\" />";
+            html += "<img src=\"chest.png\" id=\""+field.replace(/ /g,"")+j+"\" class=\"unlit-chest\" />";
             if(j%3 == 2){
                 html += "<br />"; // temp hack for not looking bad
             }
@@ -24,23 +24,32 @@ function generateChests(){
 
 //Just updates chest graphic to colored/uncolored
 function whichChest(event){
-    var changedField = event.target.parentNode.id; //Get the parentID, which should be field name
-
-    if(event.button == 0 && !event.shiftKey){ //leftclick
-        event.target.className = "unlit-chest";
-        for(var field in chestData.fields){
-            if(chestData.fields[field].name == changeField){
-                chestData.fields[field].currChests=0;
+    if (event.target.tagName == "IMG") {
+        var changedField = event.target.parentNode; //Get the parentID, which should be field name
+        var chestCount = 0;
+        if (event.button == 0 && event.shiftKey) { //leftclick
+            //event.target.className = "unlit-chest";
+            for (var field in chestData.fields) {
+                if (field == changedField.id) {
+                    chestData.fields[field].currChests = chestData.fields[field].lootChests;
+                    chestCount = chestData.fields[field].currChests;
+                }
             }
-        }  
-    } else if(event.button == 0 && event.shiftKey){
-        for(var field in chestData.fields){
-            if(chestData.fields[field].name == changeField && chestData.fields[field].currChests != chestData.fields[field].lootChests){
-                chestData.fields[field].currChests+=1;
+        } else if (event.button == 0 && !event.shiftKey) {
+            for (var field in chestData.fields) {
+                if (field == changedField.id && chestData.fields[field].currChests != 0) {
+                    chestData.fields[field].currChests -= 1;
+                    chestCount = chestData.fields[field].currChests;
+                }
             }
-        }  
-        event.target.className = "lit-chest";
+        }
+        
+        if(chestCount == 0){
+            event.target.className = "unlit-chest";
+        }else{
+            event.target.className = "lit-chest";
+        }
+        changedField.lastChild.innerHTML = chestCount;
     }
-    
       
 }
